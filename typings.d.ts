82204a7f71438af14476ddd42a4a68b5d271f0b2
Qumei <qumei.it@gmail.com>
2023-09-17 16:52:41 +0800

    fix: ChatSDK 类型报错

--- typings.d.ts
+++ typings.d.ts
@@ -1,1 +1,12 @@
 import 'umi/typings';
+
+declare global {
+  interface Navigator {
+    msSaveOrOpenBlob: (blob: Blob, fileName: string) => void;
+    browserLanguage: string;
+  }
+
+  export interface Window {
+    ChatSDK: any;
+  }
+}
